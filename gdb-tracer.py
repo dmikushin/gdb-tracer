b3829e148e5d05839721ea0e14347c214461492a
Dmitry Mikushin <dmitry@kernelgen.org>
2021-06-14 19:42:59 +0200

    Removing unused code

--- gdb-tracer.py
+++ gdb-tracer.py
@@ -6,46 +6,12 @@ try:
     import gdb
     ingdb = True
 
-    READELF_BINARY = 'readelf'
-
-    class AddSymbolFileAuto (gdb.Command):
-        def __init__(self):
-            super(AddSymbolFileAuto, self).__init__("add-symbol-file-auto", gdb.COMMAND_FILES)
-
-        def invoke(self, solibpath, from_tty):
-            import os
-            self.dont_repeat()
-            if os.path.exists(solibpath) == False:
-                gdb.write("No such file or directory: {}".format(solibpath))
-                return
-            offset = self.get_text_offset(solibpath)
-            if offset == "":
-                gdb.write('Failed to add symbols from file: {}'.format(solibpath))
-                return
-            gdb_cmd = "add-symbol-file %s %s" % (solibpath, offset)
-            gdb.execute(gdb_cmd, from_tty)
-
-        def get_text_offset(self, solibpath):
-            import subprocess
-            elfres = subprocess.check_output([READELF_BINARY, "-WS", solibpath])
-            for line in elfres.splitlines():
-                if b"] .text " in line:
-                    gdb.write('test\n')
-                    return "0x" + str(line.split()[4])
-            return "" 
-
-        def complete(self, text, word):
-            return gdb.COMPLETE_FILENAME
-
-    AddSymbolFileAuto()
-
     def stopHandler(stopEvent):
         for b in stopEvent.breakpoints:
             gdb.write('breakpoint\n')
             gdb.execute('continue')
 
     gdb.events.stop.connect (stopHandler)
-    #gdb.execute('add-symbol-file-auto lib.so')
     gdb.rbreak('foo')
     gdb.execute('run')
     gdb.execute('quit')
